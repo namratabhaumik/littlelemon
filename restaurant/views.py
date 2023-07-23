9f4326b93fb4db256b2dca2de769eef138add783
stockn22 <stockn22@gmail.com>
2023-07-23 20:57:12 +0530

    Code cleanup

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,23 +3,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
-from restaurant.models import Menu, Booking
-from rest_framework.authtoken.models import Token
+from restaurant.models import Menu
 
 # Create your views here.
 def index(request):
     return render(request, 'restaurant/index.html', {})
 
 
-#normal func
-# def get_menu():
-#     menu_list = Menu.objects.values()
-#     print(menu_list)
-#     for menu in menu_list:
-#             print(menu.get('price'))
-
-
-#rest api using get request
 def get_menu(request):
     #print(Menu.objects)
     menu_list = Menu.objects.values() #returns an itrable of dict
@@ -36,19 +26,13 @@ def get_menu(request):
     return JsonResponse({'data': list(menu_list)})
 
 
-#works only when post data payload is form-data (via UI/postman form-data type)
+#For form-data input
 @csrf_exempt
 def modify_menu(request):
     id = json.loads(request.POST.get('id'))
-    #print(id)
-    #print(type(id))
     title = json.loads(request.POST.get('title'))
-    #print(title)
     price = json.loads(request.POST.get('price'))
-    #print(price)
     inventory = json.loads(request.POST.get('inventory'))
-    #print(inventory)
-    #print(type(inventory))
 
     #saving new items + modifying saved items in django admin (db)
     new_menu = Menu(id=id, title=title, price=price, inventory=inventory)
@@ -57,19 +41,14 @@ def modify_menu(request):
     return JsonResponse({'status':'Success'})
 
 
-#works only when post data payload is JSON, can be used for building public APIs 
+#For JSON data input
 class Modify_Menu(APIView):
     def post(self,request):
         id = request.data.get('id')
-        #print(id)
         title = request.data.get('title')
-        #print(title)
         price = request.data.get('price')
-        #print(price)
         inventory = request.data.get('inventory')
-        #print(inventory)
 
-        #saving new items + modifying saved items in django admin (db)
         new_menu = Menu(id=id, title=title, price=price, inventory=inventory)
         new_menu.save()
 
